12a00f2f9216e3d50974087fd6ddc93cbc7c1f4f
maurolcsilva <maurolcsilva@gmail.com>
2013-05-14 18:48:43 -0300

    Jogando o Projeto para o Servidor

---
+++ Frigorifico/src/br/edu/entidade/BoideCorte.java
@@ -0,0 +1,19 @@
+package br.edu.entidade;
+
+public class BoideCorte {
+	private float peso;
+	private String raca;
+	
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	public String getRaca() {
+		return raca;
+	}
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+}
