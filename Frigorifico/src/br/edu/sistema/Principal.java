3133a513006c8d8ed8e65096682ba703a25ce230
maurolcsilva <maurolcsilva@gmail.com>
2013-05-14 18:53:54 -0300

    Novo

---
+++ Frigorifico/src/br/edu/sistema/Principal.java
@@ -0,0 +1,13 @@
+package br.edu.sistema;
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Usando o GIT");
+	}
+
+}
